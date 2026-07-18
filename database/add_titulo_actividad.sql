431749bf24b33b1d8b31a5f70bb7725b2f9a686b
Katherine Mendez <katherine.mendez606@gmail.com>
2026-07-18 10:53:03 -0500

    Actualización de documentos y base de datos

--- database/add_titulo_actividad.sql
+++
@@ -1,5 +0,0 @@
--- Agregar columna titulo a actividades
-ALTER TABLE actividades ADD COLUMN titulo VARCHAR(200) NOT NULL DEFAULT '' AFTER tutor_id;
-
--- Migrar datos existentes: usar los primeros 200 chars de descripcion como titulo
-UPDATE actividades SET titulo = LEFT(descripcion, 200) WHERE titulo = '';
